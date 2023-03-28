b56cece2d26b380f33106edce88167f283273ea6
unknown <krishnanapm13@gmail.com>
2023-03-28 15:19:49 +0530

    first day

---
+++ src/Components/TopNavbar/TopNavbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './TopNavbar.css'
+import Phone from '../../assets/images/phone-solid.svg'
+
+export class TopNavbar extends Component {
+  render() {
+    return (
+      <div className='top-navbar'>
+        <div className='item'>Information</div>
+        <div className='item'>
+          <div><i className='fas fa-envelope'></i> Email</div>
+          <div><img src={Phone} width="15px"></img> Phone</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TopNavbar
